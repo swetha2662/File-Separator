0a0a466d6c519e8285106f26fed5c52269f2788f
Swetha S <85669283+swetha2662@users.noreply.github.com>
2024-06-01 16:46:35 +0530

    Add files via upload

---
+++ FileSeparator.py
@@ -0,0 +1,40 @@
+import os
+try:
+    from pathlib import Path
+except:
+    os.system('python -m pip install pathlib')
+    from pathlib import Path
+
+
+subcategory={
+
+    'DOCUMENTS':['.pdf','.txt','.doc','rtf','.pptx','.docx',
+                '.odt'],
+    'AUDIO':['.mp3','.m4b','.m4a','.webvtt','.cea-608/708',
+            '.dfxp', '.sami', '.scc', '.srt', '.ttml', '.3gpp'],
+    'VIDEOS':['.mov','.avi','.mp4','.3gp','.ogg', '.oga', '.ogv',
+                '.ogx','.wmv','.webm','.flv','.avi','.QuickTime',
+                '.hdv','.mxf','.mpeg-2','.ts','.wav','.lfx',
+                '.gfx''.vob'],
+    'IMAGES':['.jpg','.jpeg','.raw','.png','.tiff','.gif',],
+    'PROGRAMMING FILES':['.htm','.html','.cpp','.c','.py','.css','.java']
+}
+def findTheCategory (value):
+    for filename,ext in subcategory.items():
+        for extension in ext:
+            if extension == value:
+                return filename
+    return 'MISC'
+def organizeDir():
+    for i in os.scandir():
+        if i.is_dir():
+            continue
+        filePath=Path(i)
+        fileType=filePath.suffix.lower()
+        directory = findTheCategory(fileType)
+        directoryPath=Path(directory)
+        if directoryPath.is_dir() !=True:
+            directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
+
+organizeDir()
